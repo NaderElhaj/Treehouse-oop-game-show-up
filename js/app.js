d4e3b834bb5f05936535346a95f2af34cec571a8
Nader Elhaj <naderelhaj3@gmail.com>
2021-05-14 16:14:48 +0200

    f

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const keys = document.querySelectorAll('.key');
-const game = new Game();
 document.getElementById("btn__reset").addEventListener('click',()=>{
+    const game = new Game();
+
     game.startGame();
     for(let key=0;key<keys.length;key++){
         keys[key].addEventListener('click',(e)=>{
